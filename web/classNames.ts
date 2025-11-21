379a39d1830a6cbcad80b776399d5102865bd1f4
cptSwing <jens@jbrandenburg.de>
2025-11-21 14:18:49 +0100

    jsdoc for classNames and looser type

--- web/classNames.ts
+++ web/classNames.ts
@@ -1,4 +1,17 @@
-function classNames(...classes: (string | undefined)[]): string {
+/**
+ * Joins strings of CSS classnames
+ *
+ * This enables ternary operations, or short-circuit evaluation for conditional application of classes.
+ *
+ * @param {...(string | false | undefined | null)[]} classes - Array of strings
+ * @returns {string} A concatenated string joining all in `classes`
+ * @example 
+ * 
+ * const foo = false;
+ * const classesString = classNames('class-one', 'class-two', foo && 'class-three'); // 'class-one class-two'
+ *
+ */
+function classNames(...classes: (string | false | undefined | null)[]): string {
     return classes.filter(Boolean).join(' ');
 }
 
